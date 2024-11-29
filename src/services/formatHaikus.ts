1a4ee10dc5b5e27e60786233e65e8dd9ccbbbe6d
Referenz <mail@referenz.io>
2024-11-29 10:35:03 +0100

    Größerer Abstand zwischen Gedicht und Hashtags

--- src/services/formatHaikus.ts
+++ src/services/formatHaikus.ts
@@ -4,6 +4,6 @@ import { dateString } from "../util/dateString.ts";
 export function getFormattedHaikus(haikuFeed: HaikuFeed): string[] {
   const date = `📰 ${dateString()}\n\n`;
   return haikuFeed.haikus.map(({ haiku, hashtags }) =>
-    `${haiku.line1}\n${haiku.line2}\n${haiku.line3}\n\n🏷️ ${hashtags.join(" ")}\n${date}`
+    `${haiku.line1}\n${haiku.line2}\n${haiku.line3}\n\n\n🏷️ ${hashtags.join(" ")}\n${date}`
   );
 }
