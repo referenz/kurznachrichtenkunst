a12b37fa267aa39e93f2901c5d2d65267512f96d
Referenz <mail@referenz.io>
2024-11-29 10:48:35 +0100

    Leerzeilen unter dem Datum  entfernt

--- src/services/formatHaikus.ts
+++ src/services/formatHaikus.ts
@@ -2,7 +2,7 @@ import type { HaikuFeed } from "../types.ts";
 import { dateString } from "../util/dateString.ts";
 
 export function getFormattedHaikus(haikuFeed: HaikuFeed): string[] {
-  const date = `📰 ${dateString()}\n\n`;
+  const date = `📰 ${dateString()}`;
   return haikuFeed.haikus.map(({ haiku, hashtags }) =>
     `${haiku.line1}\n${haiku.line2}\n${haiku.line3}\n\n\n🏷️ ${hashtags.join(" ")}\n${date}`
   );
