82cc37ef424f5f3ec2abf95a474a2a27affe831f
Referenz <mail@referenz.io>
2024-11-30 22:29:18 +0100

    Ausgelagerte Instructions werden jetzt korrekt eingelesen

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -4,10 +4,13 @@ import type { HaikuFeed } from "../types.ts";
 import { validateHaikuFeed } from "../util/validateHaikuFeed.ts";
 
 export async function generateResponse(prompt: string): Promise<HaikuFeed> {
+  const __dirname = new URL('.', import.meta.url).pathname;
+  const instruction = await Deno.readTextFile(`${__dirname}/../util/instruction.md`);
+
   const genAI = new GoogleGenerativeAI(getEnvVar("GEMINI_API"));
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
-    systemInstruction: await Deno.readTextFile('../util/instruction.md'),
+    systemInstruction: instruction,
   });
 
   const result = await model.generateContent(prompt);
