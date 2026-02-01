8da4197c3f582e5813ecf51ca8233e5d6b1987fc
referenz <thomas.u.schueler@gmail.com>
2026-02-01 11:35:56 +0100

    Tile generation deaktiviert

--- src/mainLogic.ts
+++ src/mainLogic.ts
@@ -1,41 +1,38 @@
 import { delay } from "@std/async/delay";
-import { getNews } from "./services/feeds.ts";
-import { generateResponse } from "./services/gemini.ts";
-import { getFormattedHaikus } from "./services/formatHaikus.ts";
-import { postToMastodon } from "./services/mastodon.ts";
-import type { HaikuFeed } from "./types.ts";
 import { postToBluesky } from "./services/bluesky.ts";
+import { getNews } from "./services/feeds.ts";
+import { getFormattedHaikus } from "./services/formatHaikus.ts";
+import { generateResponse } from "./services/gemini.ts";
+import { postToMastodon } from "./services/mastodon.ts";
 import { createTiles } from "./services/tile.ts";
+import type { HaikuFeed } from "./types.ts";
 
 async function postHaikus(haikus: string[]) {
-  for (const haiku of haikus) {
-    // Parallel auf Mastodon und Bluesky posten
-    await Promise.allSettled([
-      postToMastodon(haiku),
-      postToBluesky(haiku),
-    ]);
+	for (const haiku of haikus) {
+		// Parallel auf Mastodon und Bluesky posten
+		await Promise.allSettled([postToMastodon(haiku), postToBluesky(haiku)]);
 
-    // Warte 3 Sekunden
-    await delay(3000);
-  }
+		// Warte 3 Sekunden
+		await delay(3000);
+	}
 }
 
 export async function executeMainLogic() {
-  const news = await getNews();
-  if (!news || news.length === 0) {
-    console.error("Keine Nachrichten verfügbar. Abbruch.");
-    return;
-  }
+	const news = await getNews();
+	if (!news || news.length === 0) {
+		console.error("Keine Nachrichten verfügbar. Abbruch.");
+		return;
+	}
 
-  let response: HaikuFeed;
-  try {
-    response = (await generateResponse(news.join("\n"))) as HaikuFeed;
-    console.log("[INFO] Generierte Haikus:", response);
-    const formattedHaikus = getFormattedHaikus(response);
-    await postHaikus(formattedHaikus);
-    console.log("[INFO] Erfolgreich gepostet.");
-    createTiles(response);
-  } catch (err: unknown) {
-    console.error("Fehler aufgetreten: ", err);
-  }
+	let response: HaikuFeed;
+	try {
+		response = (await generateResponse(news.join("\n"))) as HaikuFeed;
+		console.log("[INFO] Generierte Haikus:", response);
+		const formattedHaikus = getFormattedHaikus(response);
+		await postHaikus(formattedHaikus);
+		console.log("[INFO] Erfolgreich gepostet.");
+		// createTiles(response);
+	} catch (err: unknown) {
+		console.error("Fehler aufgetreten: ", err);
+	}
 }
